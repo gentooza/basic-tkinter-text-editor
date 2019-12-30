0fe5127888af69baff93a110a22c8009f98dc0c4
Joa <joaquin@pixelada.org>
2019-12-30 22:06:04 +0100

    PEP8 compliance, and code cleaning

--- help_menu.py
+++ help_menu.py
@@ -1,21 +1,23 @@
-from common_imports import *
-from tkMessageBox import *
+import tkinter as tk
+from tkinter.messagebox import showinfo
 import sys
 
+
 class Help():
-	def about(root):
-		showinfo(title="About", message="This a simple text editor implemented in Python's Tkinter")
+    def about(root):
+        showinfo(title="About",
+                 message="This a simple text editor implemented in Python's Tkinter")
 
 
-def main(root,text,menubar):
+def main(root, text, menubar):
+    help = Help()
 
-	help = Help()
+    helpMenu = tk.Menu(menubar)
+    helpMenu.add_command(label="About", command=help.about)
+    menubar.add_cascade(label="Help", menu=helpMenu)
 
-	helpMenu = Menu(menubar)
-	helpMenu.add_command(label="About", command=help.about)
-	menubar.add_cascade(label="Help", menu=helpMenu)
+    root.config(menu=menubar)
 
-	root.config(menu=menubar)
 
-if __name__=="__main__":
-	print ("Please run 'main.py'")
+if __name__ == "__main__":
+    print("Please run 'main.py'")
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-from common_imports import *
-from tkFileDialog import *
-from tkMessageBox import *
-from tkFont import Font
-from ScrolledText import *
+import tkinter as tk
+from tkinter.font import Font
+from tkinter.scrolledtext import ScrolledText
 import file_menu
 import edit_menu
 import format_menu
 import help_menu
 
-root = Tk()
+root = tk.Tk()
 
 root.title("Text Editor-Untiltled")
 root.geometry("300x250+300+300")
@@ -16,14 +14,18 @@ root.minsize(width=400, height=400)
 
 font = Font(family="Verdana", size=10)
 
-text = ScrolledText(root, state='normal', height=400, width=400, wrap='word', font = font, pady=2, padx=3, undo=True)
-text.pack(fill=Y, expand=1)
+text = ScrolledText(root, state='normal',
+                    height=400, width=400,
+                    wrap='word',
+                    font=font, pady=2,
+                    padx=3, undo=True)
+text.pack(fill=tk.Y, expand=1)
 text.focus_set()
 
-menubar = Menu(root)
+menubar = tk.Menu(root)
 
-file_menu.main(root,text,menubar)
-edit_menu.main(root,text,menubar)
-format_menu.main(root,text,menubar)
-help_menu.main(root,text,menubar)
+file_menu.main(root, text, menubar)
+edit_menu.main(root, text, menubar)
+format_menu.main(root, text, menubar)
+help_menu.main(root, text, menubar)
 root.mainloop()
